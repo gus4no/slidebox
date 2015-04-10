aa32812118224d477257bfa9a67259e8ff6bc9f2
Mumo Carlos <mumo.crls@gmail.com>
2015-04-10 16:10:29 -0500

    Bugfix: Multi Slidebox navigation scope
    * Add scope to update limit statuses

--- js/slidebox.js
+++ js/slidebox.js
@@ -69,8 +69,8 @@ angular.module('Slidebox', [])
             interval = setInterval(function () {
               content.scrollLeft -= 10;
 
-              updateLeftLimitStatus();
-              updateRightLimitStatus(maxScroll);
+              updateLeftLimitStatus(this);
+              updateRightLimitStatus(maxScroll, this);
 
               if(content.scrollLeft <= limit || content.scrollLeft <= 0){
                 clearInterval(interval);
@@ -79,14 +79,14 @@ angular.module('Slidebox', [])
             }, 1);
           }
 
-          function updateLeftLimitStatus() {
-            angular.element('.slidebox-left')
-                .toggleClass('is-left-limit-reached', content.scrollLeft == 0);
+          function updateLeftLimitStatus(target) {
+            angular.element(target).
+              toggleClass('is-left-limit-reached', content.scrollLeft == 0);
           }
 
-          function updateRightLimitStatus(maxScroll) {
-            angular.element('.slidebox-right')
-                .toggleClass('is-right-limit-reached', content.scrollLeft == maxScroll);
+          function updateRightLimitStatus(maxScroll, target) {
+            angular.element(target).
+              toggleClass('is-right-limit-reached', content.scrollLeft == maxScroll);
           }
 
           function scrollRight(){
@@ -96,8 +96,8 @@ angular.module('Slidebox', [])
             interval = setInterval(function () {
               content.scrollLeft += 10;
 
-              updateLeftLimitStatus();
-              updateRightLimitStatus(maxScroll);
+              updateLeftLimitStatus(this);
+              updateRightLimitStatus(maxScroll, this);
 
               if(content.scrollLeft >= limit || content.scrollLeft >= maxScroll){
                 clearInterval(interval);
